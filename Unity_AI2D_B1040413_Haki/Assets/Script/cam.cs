d7f0df152fedf419077896c2335cf5e37466b28a
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2020-01-06 08:41:19 +0800

    0105

---
+++ Unity_AI2D_B1040413_Haki/Assets/Script/cam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class cam : MonoBehaviour
+{
+    public float camspeed = 10;
+
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 cam = transform.position;
+        Vector3 tar = player.position;
+        tar.z = -10;
+        tar.y = Mathf.Clamp(tar.y, -1, 1);
+        transform.position = Vector3.Lerp(cam, tar, 0.3f * Time.deltaTime * camspeed);
+    }
+}
